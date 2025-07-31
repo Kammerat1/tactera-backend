6a2fe9deecb2ced13eb3bd41f5eca75838e68a65
Kammerat1 <kammerat1@pm.me>
2025-07-31 13:55:25 +0200

    Renamed all /models files to follow same rules

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,34 @@
-from .manager import Manager, ManagerRegister, ManagerLogin
+# tactera_backend/models/__init__.py
+# Centralized imports for all database models and schemas
+
+# Manager
+from .manager_model import Manager, ManagerRegister, ManagerLogin
+
+# Club
 from .club_model import Club
 from .club_schemas import ClubRegister
-from .league import League
-from .player import Player
-from .player_stat import PlayerStat
-from .stat_level_requirement import StatLevelRequirement
+
+# League
+from .league_model import League
+
+# Player and stats
+from .player_model import Player
+from .player_stat_model import PlayerStat
+
+# Stat level requirements
+from .stat_level_requirement_model import StatLevelRequirement
+
+# Training
 from .training_model import TrainingGround, TrainingHistory, TrainingHistoryStat
+
+# Match and results
 from .match_model import Match, MatchResult
+
+# Season state
 from .season_model import SeasonState
+
+# Country
 from .country_model import Country
+
+# Stadium
 from .stadium_model import Stadium, StadiumPart
